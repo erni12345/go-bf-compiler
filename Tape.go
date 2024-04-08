83727dce3eb058ead922bb35f2373aab1e298fc7
Ernesto Hellouin de Menibus <E.L.M.HellouindeMenibus@student.tudelft.nl>
2024-04-08 20:20:34 +0200

    Starting with a JiT compiler / Interpreter, made first functionality of the tape

---
+++ Tape.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+    "fmt"
+)
+
+
+const tapeSize = 30000 //TODO find a way to be smarter about size?
+
+//So we are using a struct which holds an array of bytes and a pointer to simulate the Tape
+type Tape struct {
+    cells []byte
+    pointer int
+}
+
+//return a pointer because its better than copying each time
+func NewTape() *Tape{ //we return a pointer thats why we use the *
+    return &Tape{ //the & operator instructs to return a pointer
+        cells: make([]byte, tapeSize)   ,
+        pointer: 0,
+    }
+}
+
+//+
+func (t *Tape) Increment () { //hacing the (t *Tape) makes it associated with the Tape struct, meaning i can then call tape.Increment()
+    t.cells[t.pointer]++;
+}
+
+//-
+func (t *Tape) Decrement() {
+    t.cells[t.pointer]--;
+}
+
+// >
+func (t *Tape) MoveRight(){
+    t.pointer++; //TODO : WHAT IF AT THE END, possible solution -> make tape bigger and copy
+}
+
+// <
+func (t *Tape) MoveLeft(){
+    t.pointer--; //TODO: what if at pos 0?
+}
+
+//.
+func (t *Tape) OutputPointer(){
+    fmt.Println(string(t.cells[t.pointer]));
+}
+
+//,
+func (t *Tape) InputToPointer(){
+    var input byte;
+    fmt.Scanf("%c\n", &input)
+    t.cells[t.pointer] = input
+}
+
+
+
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+
+import(
+    "fmt"
+)
+
+func main() {
+    tape := NewTape()
+    fmt.Println("Tape Pointer:", tape.pointer)
+}
