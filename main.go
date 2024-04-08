d327e0392de3bfe0827c2dcde972e74fe04d979a
Ernesto Hellouin de Menibus <E.L.M.HellouindeMenibus@student.tudelft.nl>
2024-04-08 23:55:50 +0200

    added a timer

--- main.go
+++ main.go
@@ -4,18 +4,25 @@ package main
 import(
     "fmt"
     "os"
+    "time"
 )
 
 func main() {
-
-    //checking if we have a file name
-    if len(os.Args) < 2 {
-        fmt.Println("Usage : bf-compiler <filename>")
+    // Checking if we have a file name
+    if len(os.Args) < 3 {
+        fmt.Println("Usage : bf-compiler <filename> <mode>")
         return
     }
 
     filePath := os.Args[1]
-    Interpret(filePath)
+    mode := os.Args[2]
+
+    if mode == "interpret" {
+        start := time.Now()
+        Interpret(filePath)
+        elapsed := time.Since(start)
+        fmt.Printf("Mode: %s, Time taken: %s\n", mode, elapsed)
+    }
 }
 
 func must(err error){
