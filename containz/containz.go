26b7e7550a4a33a105390d7872041de73ba19e9e
Ivan Audouard <i.audouard22@ejm.org>
2024-04-14 19:41:01 +0200

    feat: refactored into two commands

--- containz/containz.go
+++ containz/containz.go
@@ -12,7 +12,7 @@ import (
 func main() {
 	res, err := Run("example")
 	if err != nil {
-		log.Fatalf("epic fail %s", err)
+		log.Fatal(err)
 	}
 	log.Print(res)
 }
@@ -20,8 +20,9 @@ func main() {
 // returns the output logs
 func Run(code string) (string, error) {
 	// if err := os.WriteFile("./docker/main.go", []byte(code), 0644); err != nil {
-	// 	return "", errors.New("could not write to file")
+	// 	return "", err
 	// }
+
 	cwd, err := os.Getwd()
 	if err != nil {
 		return "", err
@@ -31,22 +32,32 @@ func Run(code string) (string, error) {
 
 	t := fmt.Sprint(time.Now().Unix())
 
-	// cmd := exec.Command("docker", "run", "--rm", "-it", "$(docker", "build", "-q", ".")
-	cmd := exec.Command("docker", "build", ".", "-t", t)
-	if err = cmd.Run(); err != nil {
-		log.Println("build")
+	if err = build(t); err != nil {
 		return "", err
 	}
 
-	cmd = exec.Command("docker", "run", "--name", t, t)
-	if err = cmd.Run(); err != nil {
-		log.Println("run")
-		return "", err
-	}
-
-	out, err := exec.Command("docker", "logs", t).Output()
+	out, err := run(t)
 	if err != nil {
-		log.Println("logs")
+		return "", err
+	}
+
+	return out, nil
+}
+
+func build(tag string) error {
+	// TODO do it with this command
+	// cmd := exec.Command("docker", "run", "--rm", "-it", "$(docker", "build", "-q", ".")
+	cmd := exec.Command("docker", "build", ".", "-t", tag)
+	if err := cmd.Run(); err != nil {
+		log.Println("build")
+		return err
+	}
+	return nil
+}
+
+func run(tag string) (string, error) {
+	out, err := exec.Command("docker", "run", tag).Output()
+	if err != nil {
 		return "", err
 	}
 	return string(out), nil
